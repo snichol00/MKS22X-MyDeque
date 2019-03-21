7af537ea377f5d509a17bf49483018b86d00b941
Sophie Nichol <snichol00@stuy.edu>
2019-03-21 09:59:48 -0400

    made add methods

--- MyDeque.java
+++ MyDeque.java
@@ -3,11 +3,15 @@ public class MyDeque<E>{
   private int size, start, end;
 
   public MyDeque(){
-
+    @SuppressWarnings("unchecked")
+    E[] d = (E[])new Object[10];
+    data = d;
   }
 
   public MyDeque(int initialCapacity){
-
+    @SuppressWarnings("unchecked")
+    E[] d = (E[])new Object[initialCapacity];
+    data = d;
   }
 
   public int size(){
@@ -23,11 +27,33 @@ public class MyDeque<E>{
   }
 
   public void addFirst(E element){
-
+    if (end == start - 1){
+      resize();
+      addFirst(element);
+    }
+    else if (start == 0){
+      start = data.length - 1;
+      data[start] = element;
+    }
+    else{
+      start--;
+      data[start] = element;
+    }
   }
 
   public void addLast(E element){
-
+    if (start == end - 1){
+      resize();
+      addLast(element);
+    }
+    else if (end == data.length - 1){
+      end = 0;
+      data[end] = element;
+    }
+    else{
+      end++;
+      data[end] = element;
+    }
   }
 
   public E removeFirst(){
